301e7d57dcb1f1b25d38eb84cdcc04d58b10f854
vladis-garagashev <vladislav.garagashev@gmail.com>
2026-05-26 11:16:19 +0500

    week 1 day 1

---
+++ src/AboutMe.java
@@ -0,0 +1,17 @@
+public class AboutMe {
+    public static void aboutMe(String[] args) {
+
+        String name = "Владислав";
+        int age = 30;
+        String city = "Екатеринбург";
+        String hobby = "Гейминг";
+        String goal = "Работа в IT";
+
+        System.out.println("Имя: " + name);
+        System.out.println("Возраст: " + age);
+        System.out.println("Город: " + city);
+        System.out.println("Любимое занятие : " + hobby);
+        System.out.println("Цель изучения Java : " + goal);
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, Java!");
+
+        String name = "Владислав";
+        int age = 30;
+        double height = 186.1;
+        boolean likesProgramming = true;
+
+        System.out.println("Имя: " + name);
+        System.out.println("Возраст: " + age);
+        System.out.println("Рост: " + height);
+        System.out.println("Люблю программирование: " + likesProgramming);
     }
 }
